ba53b5718d19a740b1c521000edf486936cfd6bf
oscar89 <oskr_89@live.com.ar>
2024-03-11 18:10:03 +0100

    Agregando mejorando modelo Usuario

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from models import Usuario
 
 app = Flask(__name__)
+app.config.from_object("config")
 
-@app.route('/')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+@app.route("/")
 def inicio():
-    return render_template('base.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
