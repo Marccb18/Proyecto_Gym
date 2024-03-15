99ae5d2d83c0b435c417c3e25802a660bf2b8fb6
oscar89 <oskr_89@live.com.ar>
2024-03-15 18:27:19 +0100

    oscar

--- app.py
+++ app.py
@@ -4,10 +4,28 @@ from flask_login import LoginManager
 from models import Usuario
 
 app = Flask(__name__)
-app.config.from_object("config")
 
+# Configuración de la base de datos
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "sqlite:///gym.db"  # Cambia esto según tu configuración
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Inicializa la extensión SQLAlchemy
 db = SQLAlchemy(app)
+
 login_manager = LoginManager(app)
+
+# Asigna la vista de login requerida
+login_manager.login_view = "iniciar"
+
+# Inicializa la extensión LoginManager
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
 
 
 @app.route("/")
@@ -15,5 +33,26 @@ def inicio():
     return render_template("index.html")
 
 
+@app.route("/iniciar")
+def iniciar():
+    return render_template("iniciar.html")
+
+
+@app.route("/registrar")
+def registrar():
+    return render_template("registrar.html")
+
+
+@app.route("/cerrar")
+def cerrar():
+    return render_template("cerrar.html")
+
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
+
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
