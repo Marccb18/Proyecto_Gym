27760ba430cb3781c0b9d070b89440d972f29345
oscar89 <oskr_89@live.com.ar>
2024-03-18 19:28:47 +0100

    Registro

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from forms import RegistroForm
 from models import Usuario
 
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "tu_clave_secreta_aqui"
 
 # Configuración de la base de datos
 app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -16,6 +21,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
+
 
 # Asigna la vista de login requerida
 login_manager.login_view = "iniciar"
@@ -39,11 +45,33 @@ def iniciar():
     return render_template("login_form.html")
 
 
-@app.route("/registrar", methods=["POST"])
+@app.route("/registrar", methods=["POST", "GET"])
 def registrar():
+    form = RegistroForm()  # Crear una instancia del formulario
     if request.method == "POST":
-        PASS
-    return render_template("signup_form.html")
+        # Obtiene los datos del formulario
+        usuario = request.form.get("usuario")
+        password = request.form.get("password")
+
+        # Corroborar si existe el usuario
+        existe_usuario = Usuario.query.filter_by(usuario=usuario).first()
+
+        if existe_usuario:
+            flash("El usuario ya existe.")
+            return redirect(url_for("registrar"))
+
+        # Crea un nuevo ususario
+        nuevo_usuario = Usuario(usuario=usuario)
+        nuevo_usuario.set_password(password)
+
+        # Guardar usuario en la base de datos
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+
+        flash("Se creó el nuevo usuario")
+        return redirect(url_for(iniciar))
+
+    return render_template("signup_form.html", form=form)
 
 
 @app.route("/cerrar")
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    usuario = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
